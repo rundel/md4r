1a9ab97f80b9453f46843de40b924c39d61d241a
Colin Rundel <rundel@gmail.com>
2021-02-18 13:38:42 +0000

    Some basic utility functions

---
+++ R/md_util.R
@@ -0,0 +1,38 @@
+#' @export
+md_n_nodes = function(x) {
+  checkmate::assert_class(x, "md_node")
+
+  length(unlist(x))
+}
+
+#' @export
+md_text = function(x, type = "md_text") {
+  checkmate::assert_class(x, "md_node")
+
+  # This is an ugly hack
+  map_ast(x, classes = type)
+}
+
+#' @export
+md_text_summary = function(x, max_width = 60, type = "md_text_normal", replace_newline = "\\\\n") {
+  txt = md_text(x, type)
+  txt = paste(txt, collapse="\n")
+  txt = gsub("\\n", replace_newline, txt)
+  txt = substr(txt, 0, max_width)
+
+  if (nchar(txt) == max_width)
+    txt = paste0(txt, ellipsis_chars())
+
+  txt
+}
+
+ellipsis_chars = function() {
+  if (is_utf8_output()) {
+    "…"
+  } else {
+    "..."
+  }
+}
+
+
+
