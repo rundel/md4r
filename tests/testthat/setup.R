8fb74e13266c90ee80534785b5271a564d009300
Colin Rundel <rundel@gmail.com>
2021-03-12 14:25:52 +0000

    Current testing setup

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,3 +1,29 @@
+
+# From https://github.com/r-lib/cli/issues/226
+
+local_cli_config = function(
+  unicode = FALSE, dynamic = FALSE, ansi = FALSE,
+  num_colors = 1, .local_envir = parent.frame()
+) {
+  withr::local_options(
+    cli.dynamic = dynamic,
+    cli.ansi = ansi,
+    cli.unicode = unicode,
+    crayon.enabled = num_colors > 1,
+    crayon.colors = num_colors,
+    .local_envir = .local_envir
+  )
+  withr::local_envvar(
+    PKG_OMIT_TIMES = "true",
+    PKG_OMIT_SIZES = "true",
+    .local_envir = .local_envir
+  )
+}
+
+
+
+######
+
 clean_html = function(x) {
   x = paste(x, collapse="")
   x = gsub("\n", "", x)
@@ -25,18 +51,132 @@ expect_identical_html = function(md, flags, expected, info = NULL, ...) {
     sep="\n"
   )
 
-  #expect(
-  #  length(comp) == 0,
-  #  msg,
-  #  trace = "",
-  #  trace_env = rlang::caller_env()
-  #)
+  type = if (length(comp) == 0) "success"
+  else "failure"
+
+  exp_signal(
+    expectation(type, msg, srcref = NULL, trace = NULL)
+  )
+
+  invisible()
+}
+
+
+
+
+expect_identical_md = function(md, md_ast, exp_ast, flags, info = NULL, ...) {
+  # Based on testthat:::expect_waldo_equal
+  comp = testthat:::waldo_compare(md_ast, exp_ast, ..., x_arg = "actual", y_arg = "expected")
+
+  local_cli_config(unicode = TRUE, num_colors = 256)
+
+  msg = paste(
+    "",
+    "Generated markdown ast does not match expected ast.",
+    "",
+    cli::style_bold("markdown:"),
+    cli::col_grey( paste(
+      md, collapse="\n"
+    ) ),
+    "",
+    cli::style_bold("expected ast:"),
+    paste(
+      capture.output(print(exp_ast)),
+      collapse="\n"
+    ),
+    "",
+    cli::style_bold("observed ast:"),
+    paste(
+      capture.output(print(md_ast)),
+      collapse="\n"
+    ),
+    sep="\n"
+  )
 
   type = if (length(comp) == 0) "success"
   else "failure"
 
-  exp <- expectation(type, msg, srcref = NULL, trace = NULL)
-  exp_signal(exp)
+  exp_signal(
+    expectation(type, msg, srcref = NULL, trace = NULL)
+  )
 
   invisible()
+}
+
+
+
+
+
+#######################
+###                 ###
+### md4c test tools ###
+###                 ###
+#######################
+
+read_md4c_tests = function(file) {
+  section = character()
+  flags = character()
+  examples = list()
+
+  in_example = FALSE
+  end_md = FALSE
+  md = character()
+  html = character()
+
+  flag_pat = "(MD_FLAG_|MD_DIALECT_)[A-Z]+"
+  chunk_start_pat = "^```````````````````````````````` example$"
+  chunk_end_pat   = "^````````````````````````````````$"
+
+  l = readLines(file)
+  for(i in seq_along(l)) {
+    line = l[i]
+
+    if (grepl("^#{1,6}", line)) {
+      section = sub("^#{1,6}\\s+","", line)
+    }
+    else if (grepl(flag_pat, line)) {
+      m = regexpr(flag_pat, line)
+      flags = c(flags, regmatches(line,m))
+    }
+    else if (grepl(chunk_start_pat, line)) {
+      in_example = TRUE
+      line_start = i+1
+    }
+    else if (grepl(chunk_end_pat, line)) {
+      in_example = FALSE
+      end_md = FALSE
+
+      examples[[length(examples)+1]] = list(
+        md = md,
+        html = html,
+        sec = section,
+        line_start = line_start,
+        line_end = i-1
+      )
+
+      md = character()
+      html = character()
+    }
+    else if (grepl("^\\.$", line)) {
+      end_md = TRUE
+    } else {
+      if (in_example & !end_md)
+        md = c(md, line)
+      if (in_example & end_md)
+        html = c(html, line)
+    }
+  }
+
+  if (grepl("wiki-links", file)) {
+    # Required flag not listed in the file for some reason
+    flags = c(flags, "MD_FLAG_TABLES")
+  }
+
+
+  list(
+    file = file,
+    name = gsub("^.*/|\\.txt$","", file),
+    flags = flags,
+    examples = examples
+  )
 }
