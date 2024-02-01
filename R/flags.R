e557f4283fcd5563092f26704120862a68a22133
Colin Rundel <rundel@gmail.com>
2024-02-01 13:16:14 -0500

    move flag def

--- R/flags.R
+++ R/flags.R
@@ -1,26 +1,3 @@
-md4c_flags = tibble::tribble(
-  ~flag,                              ~description,
-  "MD_FLAG_COLLAPSEWHITESPACE",       "In MD_TEXT_NORMAL, collapse non-trivial whitespace into single ' '",
-  "MD_FLAG_PERMISSIVEATXHEADERS",     "Do not require space in ATX headers ( ###header )",
-  "MD_FLAG_PERMISSIVEURLAUTOLINKS",   "Recognize URLs as autolinks even without '<', '>'",
-  "MD_FLAG_PERMISSIVEEMAILAUTOLINKS", "Recognize e-mails as autolinks even without '<', '>' and 'mailto:'",
-  "MD_FLAG_NOINDENTEDCODEBLOCKS",     "Disable indented code blocks. (Only fenced code works.)",
-  "MD_FLAG_NOHTMLBLOCKS",             "Disable raw HTML blocks.",
-  "MD_FLAG_NOHTMLSPANS",              "Disable raw HTML (inline).",
-  "MD_FLAG_TABLES",                   "Enable tables extension.",
-  "MD_FLAG_STRIKETHROUGH",            "Enable strikethrough extension.",
-  "MD_FLAG_PERMISSIVEWWWAUTOLINKS",   "Enable WWW autolinks (even without any scheme prefix, if they begin with 'www.')",
-  "MD_FLAG_TASKLISTS",                "Enable task list extension.",
-  "MD_FLAG_LATEXMATHSPANS",           "Enable $ and $$ containing LaTeX equations.",
-  "MD_FLAG_WIKILINKS",                "Enable wiki links extensn.",
-  "MD_FLAG_UNDERLINE",                "Enable underline extension (and disables '_' for normal emphasis).",
-  "MD_FLAG_HARD_SOFT_BREAKS",         "Force all soft breaks to act as hard breaks.",
-  "MD_FLAG_PERMISSIVEAUTOLINKS",      "Combines flags (MD_FLAG_PERMISSIVEEMAILAUTOLINKS | MD_FLAG_PERMISSIVEURLAUTOLINKS | MD_FLAG_PERMISSIVEWWWAUTOLINKS)",
-  "MD_FLAG_NOHTML",                   "Combines flags (MD_FLAG_NOHTMLBLOCKS | MD_FLAG_NOHTMLSPANS)",
-  "MD_DIALECT_COMMONMARK",            "All flags off",
-  "MD_DIALECT_GITHUB",                "Combines flags (MD_FLAG_PERMISSIVEAUTOLINKS | MD_FLAG_TABLES | MD_FLAG_STRIKETHROUGH | MD_FLAG_TASKLISTS)"
-)
-
 #' @name flags
 #' @rdname flags
 #'
@@ -45,13 +22,34 @@ NULL
 #' @rdname flags
 #' @export
 flags_available = function() {
-  md4c_flags[["flag"]]
+  flags_describe()[["flag"]]
 }
 
 #' @rdname flags
 #' @export
 flags_describe = function() {
-  md4c_flags
+  tibble::tribble(
+    ~flag,                              ~description,
+    "MD_FLAG_COLLAPSEWHITESPACE",       "In MD_TEXT_NORMAL, collapse non-trivial whitespace into single ' '",
+    "MD_FLAG_PERMISSIVEATXHEADERS",     "Do not require space in ATX headers ( ###header )",
+    "MD_FLAG_PERMISSIVEURLAUTOLINKS",   "Recognize URLs as autolinks even without '<', '>'",
+    "MD_FLAG_PERMISSIVEEMAILAUTOLINKS", "Recognize e-mails as autolinks even without '<', '>' and 'mailto:'",
+    "MD_FLAG_NOINDENTEDCODEBLOCKS",     "Disable indented code blocks. (Only fenced code works.)",
+    "MD_FLAG_NOHTMLBLOCKS",             "Disable raw HTML blocks.",
+    "MD_FLAG_NOHTMLSPANS",              "Disable raw HTML (inline).",
+    "MD_FLAG_TABLES",                   "Enable tables extension.",
+    "MD_FLAG_STRIKETHROUGH",            "Enable strikethrough extension.",
+    "MD_FLAG_PERMISSIVEWWWAUTOLINKS",   "Enable WWW autolinks (even without any scheme prefix, if they begin with 'www.')",
+    "MD_FLAG_TASKLISTS",                "Enable task list extension.",
+    "MD_FLAG_LATEXMATHSPANS",           "Enable $ and $$ containing LaTeX equations.",
+    "MD_FLAG_WIKILINKS",                "Enable wiki links extensn.",
+    "MD_FLAG_UNDERLINE",                "Enable underline extension (and disables '_' for normal emphasis).",
+    "MD_FLAG_HARD_SOFT_BREAKS",         "Force all soft breaks to act as hard breaks.",
+    "MD_FLAG_PERMISSIVEAUTOLINKS",      "Combines flags (MD_FLAG_PERMISSIVEEMAILAUTOLINKS | MD_FLAG_PERMISSIVEURLAUTOLINKS | MD_FLAG_PERMISSIVEWWWAUTOLINKS)",
+    "MD_FLAG_NOHTML",                   "Combines flags (MD_FLAG_NOHTMLBLOCKS | MD_FLAG_NOHTMLSPANS)",
+    "MD_DIALECT_COMMONMARK",            "All flags off",
+    "MD_DIALECT_GITHUB",                "Combines flags (MD_FLAG_PERMISSIVEAUTOLINKS | MD_FLAG_TABLES | MD_FLAG_STRIKETHROUGH | MD_FLAG_TASKLISTS)"
+  )
 }
 
 #' @rdname flags
@@ -72,7 +70,6 @@ flags_used = function(md) {
 }
 
 
-
 flags_check = function(flags, match_case = FALSE) {
   checkmate::assert_character(flags, min.len=0, any.missing = FALSE)
 
@@ -80,7 +77,7 @@ flags_check = function(flags, match_case = FALSE) {
   if (!match_case)
     flags = toupper(flags)
 
-  missing = f[ !flags %in% md4c_flags[["flag"]] ]
+  missing = f[ !flags %in% flags_describe()[["flag"]] ]
 
   if (length(missing) != 0) {
     cli::cli_abort("Invalid flag{?s} provided: {.val {missing}}")
@@ -88,13 +85,4 @@ flags_check = function(flags, match_case = FALSE) {
 
   invisible()
 }
-
-
-
-
-
-
-
-
-
 
