e258b151eb7832b059ad347897f6c2ec735e86c9
Kirill Müller <kirill@cynkra.com>
2024-02-19 19:26:37 +0100

    Move test code to sources

--- R/tests.R
+++ R/tests.R
@@ -346,3 +346,287 @@ read_gfm_tests = function(file) {
     }
   )
 }
+
+#################
+###           ###
+### gfm tests ###
+###           ###
+#################
+
+gfm_tests_to_md = function() {
+  file = system.file("specs/gfm/spec.txt", package="md4r")
+  tests = read_gfm_tests(file)
+
+  # Seem to be essentially the same issues as for CommonMark
+  skip_tests = tibble::tribble(
+    ~ex, ~msg,
+    235, "Ambiguous list to code block case #235",
+    293, "Ambiguous list to code block case #293",
+    528, "Ambiguous normal text escaping #528",
+    623, "Ambiguous normal text escaping #623",
+    624, "Ambiguous normal text escaping #624",
+    631, "Ambiguous normal text escaping #631"
+  )
+
+  purrr::iwalk(
+    tests,
+    function(test, i) {
+      section = paste(test$sec, collapse = " > ")
+      label = glue::glue("gfm - Ex {i} (L{test$line_start}-{test$line_end}) - {section}")
+      url = glue::glue("https://github.github.com/gfm/#example-{i}")
+
+      test_that(label, {
+
+        if (test$disabled) {
+          testthat::skip("Disabled test(s)")
+        }
+
+        sub = (i == skip_tests[["ex"]])
+        if (any(sub)) {
+          testthat::skip( paste0(
+            "gfm #",
+            skip_tests[["ex"]][sub], " - ",
+            skip_tests[["msg"]][sub]
+          ) )
+        }
+        flags = "MD_DIALECT_GITHUB"
+
+        expect_identical_md(
+          md = test$md, flags = flags, info = url
+        )
+      })
+    }
+  )
+}
+
+##################
+###            ###
+### md4c tests ###
+###            ###
+##################
+
+md4c_tests_to_md = function() {
+
+  tests = purrr::map(
+    list.files(
+      system.file("specs/md4c/", package="md4r"),
+      ".txt", full.names = TRUE
+    ),
+    read_md4c_tests
+  )
+
+
+  skip_tests = list(
+    "regressions" = tibble::tribble(
+      ~ex, ~msg,
+       51, "Ambiguous punct escaping #51"
+    ),
+    "spec-permissive-autolinks" = tibble::tribble(
+      ~ex, ~msg,
+        5, "Ambiguous punct escaping #5"
+    ),
+    spec = tibble::tribble(
+      ~ex, ~msg,
+      257, "Ambiguous list to code block case #257",
+      313, "Ambiguous list to code block case #313",
+      520, "Ambiguous punct escaping #520"
+    )
+
+  )
+
+  run_tests = function(file, name, flags, examples) {
+    purrr::iwalk(
+      examples,
+      function(test, i) {
+        label = glue::glue("md4c tests - {name} - Ex {i} (L{test$line_start}-{test$line_end}) - {test$sec}")
+
+        test_that(label, {
+
+          # Check skips
+          sub = (i == skip_tests[[name]][["ex"]])
+          if (any(sub)) {
+            testthat::skip( paste0(
+              name, " #",
+              skip_tests[[name]][["ex"]][sub], " - ",
+              skip_tests[[name]][["msg"]][sub]
+            ) )
+          }
+
+          expect_identical_md(
+            md = test$md, flags = flags, info = NULL
+          )
+        })
+      }
+    )
+  }
+
+  purrr::walk(
+    tests,
+    do.call, what = run_tests
+  )
+}
+
+#################
+###           ###
+### gfm tests ###
+###           ###
+#################
+
+gfm_tests_to_html = function() {
+  file = system.file("specs/gfm/spec.txt", package="md4r")
+  tests = read_gfm_tests(file)
+
+  skip_tests = tibble::tribble(
+    ~ex, ~msg,
+    203, "md4c bug - should not be a table",
+
+    345, "Minor spacing difference with code span",
+    347, "Minor spacing difference with code span",
+
+    398, "Nested strong tags not being simplified",
+    426, "Preference given to md4c test",
+    434, "Nested strong tags not being simplified",
+    435, "Nested strong tags not being simplified",
+    436, "Nested strong tags not being simplified",
+
+    473, "Preference given to md4c test",
+    474, "Preference given to md4c test",
+    475, "Preference given to md4c test",
+    477, "Preference given to md4c test",
+
+    616, "Seems like a md4c bug - should not be an autolink",
+    619, "Seems like a md4c bug - should not be an autolink",
+    620, "Seems like a md4c bug - should not be an autolink",
+
+    625, "Seems like a md4c bug - doesn't treat the trailing '+ok' as part of the url",
+    626, "Seems like a md4c bug - doesn't find the 2nd url",
+    627, "Seems like a md4c bug - doesn't find the url",
+
+    652, "Not sure what to do about tag filtering atm - FIXME",
+
+    660, "Minor spacing difference with code span"
+  )
+
+  purrr::iwalk(
+    tests,
+    function(test, i) {
+      section = paste(test$sec, collapse = " > ")
+      label = glue::glue("gfm - Ex {i} (L{test$line_start}-{test$line_end}) - {section}")
+      url = glue::glue("https://github.github.com/gfm/#example-{i}")
+
+      test_that(label, {
+
+        if (test$disabled) {
+          testthat::skip("Disabled test(s)")
+        }
+
+        sub = (i == skip_tests[["ex"]])
+        if (any(sub)) {
+          testthat::skip( paste0(
+            "gfm #", skip_tests[["ex"]][sub],
+            " - ", skip_tests[["msg"]][sub]
+          ) )
+        }
+
+        expect_identical_html(
+          test$md, "MD_DIALECT_GITHUB",
+          test$html,
+          info = label,
+          url = url
+        )
+      })
+    }
+  )
+}
+
+##################
+###            ###
+### md4c tests ###
+###            ###
+##################
+
+
+flag_lookup = c(
+  "--fcollapse-whitespace"         = "MD_FLAG_COLLAPSEWHITESPACE",
+  "--ftables"                      = "MD_FLAG_TABLES",
+  "--fpermissive-url-autolinks"    = "MD_FLAG_PERMISSIVEURLAUTOLINKS",
+  "--fstrikethrough"               = "MD_FLAG_STRIKETHROUGH",
+  "--ftables"                      = "MD_FLAG_TABLES",
+  "--fwiki-links"                  = "MD_FLAG_WIKILINKS",
+  "--fhard-soft-breaks"            = "MD_FLAG_HARD_SOFT_BREAKS",
+  "--flatex-math"                  = "MD_FLAG_LATEXMATHSPANS",
+  "--fpermissive-email-autolinks"  = "MD_FLAG_PERMISSIVEEMAILAUTOLINKS",
+  "--fpermissive-www-autolinks"    = "MD_FLAG_PERMISSIVEWWWAUTOLINKS",
+  "--ftasklists"                   = "MD_FLAG_TASKLISTS",
+  "--funderline"                   = "MD_FLAG_UNDERLINE",
+  "--fwiki-links"                  = "MD_FLAG_WIKILINKS"
+)
+
+md4c_tests_to_html = function() {
+
+  tests = purrr::map(
+    list.files(
+      system.file("specs/md4c/", package="md4r"),
+      ".txt", full.names = TRUE
+    ),
+    read_md4c_tests
+  )
+
+  skip_tests = list(
+    "coverage" = tibble::tribble(
+      ~ex, ~msg,
+      8, "Weird utf issue with spaces",
+      9, "Weird utf issue with spaces",
+      10, "Weird utf issue with spaces"
+    ),
+    "regressions" = tibble::tribble(
+      ~ex, ~msg,
+      5, "New line vs space issue"
+    ),
+    "spec" = tibble::tribble(
+      ~ex, ~msg,
+      335, "Slight spacing discrepancy in the span output",
+      337, "Slight spacing discrepancy in the span output",
+      640, "Slight spacing discrepancy in the span output",
+    )
+  )
+
+  run_tests = function(file, name, flags, examples) {
+    purrr::iwalk(
+      examples,
+      function(test, i) {
+        label = glue::glue("{name} - Ex {i} (L{test$line_start}-{test$line_end}) - {test$sec}")
+
+        test_that(label, {
+
+          sub = (i == skip_tests[[name]][["ex"]])
+          if (any(sub)) {
+            testthat::skip( paste0(
+              name, " #",
+              skip_tests[[name]][["ex"]][sub], " - ",
+              skip_tests[[name]][["msg"]][sub]
+            ) )
+          }
+
+
+          if (length(test$md) == 1)
+            test$md = paste0(test$md, "\n")
+
+          other_flags = flag_lookup[test$other]
+          stopifnot(all(!is.na(other_flags)))
+
+          expect_identical_html(
+            test$md, c(other_flags, flags),
+            test$html,
+            info = label
+          )
+        })
+      }
+    )
+  }
+
+  purrr::walk(
+    tests,
+    do.call, what = run_tests
+  )
+}
